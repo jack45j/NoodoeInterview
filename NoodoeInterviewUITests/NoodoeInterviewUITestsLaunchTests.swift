d02f53b97041fde8a96031cd0fd7eefdb603e622
20001107 - Benson Lin <benson_lin@books.com.tw>
2023-02-03 16:28:55 +0800

    Initial Commit

---
+++ NoodoeInterviewUITests/NoodoeInterviewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NoodoeInterviewUITestsLaunchTests.swift
+//  NoodoeInterviewUITests
+//
+//  Created by 林翌埕-20001107 on 2023/2/3.
+//
+
+import XCTest
+
+final class NoodoeInterviewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
